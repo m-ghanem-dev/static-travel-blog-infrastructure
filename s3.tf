5fc2ccb958a2c240ba0e6625f0311726dc427564
Mohamed Ghanem <mohamedyehia1998@gmail.com>
2025-07-27 01:24:07 +0200

    chore: s3 bucket config

--- s3.tf
+++ s3.tf
@@ -4,5 +4,37 @@ provider "aws" {
 
 resource "aws_s3_bucket" "travel_blog" {
   bucket = "static-travel-blog-bucket-jskldjlq341"
-  acl    = "private"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.travel_blog.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.travel_blog.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.travel_blog.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.travel_blog.arn}/*"
+      }
+    ]
+  })
 }
